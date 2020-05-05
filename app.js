fed2819ab764cb8130a4cde560ed6759e6aa86ea
dianestephani <diane.stephani@gmail.com>
2020-05-05 14:50:58 -0700

    following code review

--- app.js
+++ app.js
@@ -1,24 +1,64 @@
-var customersSeattle = {
-  minimum: 23,
-  maximum: 65,
-  averageCookie: 6.3,
-  getRandomIntInclusive: function() {
-    var min = this.minimum;
-    var max = this.maximum;
-    var averageCookie = 6.3;
-    return Math.floor((Math.random() * (max - min + 1) + min) * averageCookie);
-    },
+'use strict';
+
+var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+
+var seattle = {
+  name: 'seattle',
+  minCustomersHour: 23,
+  maxCustomersHour: 65,
+  averageCookiesPerCustomer: 6.3,
+  customersPerHour: [],
+  cookiesSoldPerHour: [],
+  totalCookiesPerDay: 0,
+
+  calcCustomersPerHour: function(){
+    for(var i = 0; i < hours.length; i++){
+      var customersThisHour = getRandomNumber(this.minCustomersEachHour, this.maxCustomersEachHour);
+      
+      this.customersPerHour.push(customersThisHour);
+    }
+  },
+  calcCookiesSoldPerHour: function(){
+    for(var i=0; i<this.customersPerHour.length; i++){
+      var wholeCookiesSoldForOneHour = Math.ceil(this.customersPerHour[i] * this.averageCookiesPerCustomer);
+
+      this.cookiesSoldPerHour.push(wholeCookiesSoldForOneHour);
+
+      this.totalCookiesPerDay += wholeCookiesSoldForOneHour;
+    }
+  },
+
+  render: function(){
+    seattle.calcCustomersPerHour();
+    seattle.calcCookiesSoldPerHour();
+    var seattleElement = document.getElementById('seattle');
+
+    var listItem = document.createElement('li');
+    listItem.textContent = this.name;
+    seattleElement.appendChild(listItem);
+
+
+// var customersSeattle = {
+//   minimum: 23,
+//   maximum: 65,
+//   averageCookie: 6.3,
+//   getRandomIntInclusive: function() {
+//     var min = this.minimum;
+//     var max = this.maximum;
+//     var averageCookie = 6.3;
+//     return Math.floor((Math.random() * (max - min + 1) + min) * averageCookie);
+//     },
   
-};
+// };
 
-console.log(customersSeattle.getRandomIntInclusive());
+// console.log(customersSeattle.getRandomIntInclusive());
 
-function hourlySales() {
-  for (i = 0; i < 13; i++){
-    console.log();
-  }
-}
-hourlySales();
+// function hourlySales() {
+//   for (i = 0; i < 13; i++){
+//     console.log(customersSeattle.getRandomIntInclusive);
+//   }
+// }
+// hourlySales();
 
 // determine average cookies sold per hour, call getrandominclusive inside next method
 // create for loop
@@ -47,3 +87,4 @@ hourlySales();
 //   maximum: 16, 
 //   averageCookie: 4.6
 // }
+  
